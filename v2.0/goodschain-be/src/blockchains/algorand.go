7218237f0971f59a11f7dc7fb752444f2ec1136b
Matteo Barone <matteo.barone1199@gmail.com>
2024-06-28 22:52:57 +0200

    added blockchain placeholders

--- v2.0/goodschain-be/src/blockchains/algorand.go
+++ v2.0/goodschain-be/src/blockchains/algorand.go
@@ -1,1 +1,5 @@
 package blockchains
+
+func SetupBlockchain() {
+
+}
--- v2.0/goodschain-be/src/main.go
+++ v2.0/goodschain-be/src/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"net/http"
 
+	"github.com/MatBarons/supplychain_tracking_system/blockchains"
 	"github.com/MatBarons/supplychain_tracking_system/databases"
 	"github.com/MatBarons/supplychain_tracking_system/routes"
 )
 
 func main() {
 	databases.SetupDB()
+	blockchains.SetupBlockchain()
 	router := http.NewServeMux()
 	requestsRouter := routes.CreateRequestsRoute()
 	usersRouter := routes.CreateUsersRoute()
